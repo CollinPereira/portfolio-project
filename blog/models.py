39363575f55609a93033cba0e25d93a70ff0cdb0
CollinPereira <collinp26@gmail.com>
2019-02-05 21:12:33 +0530

    individual blogs on id

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,10 @@ class Blog(models.Model):
     pub_date=models.DateTimeField()
     body=models.TextField()
     image=models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        sum=self.body[:100]
+        return sum
